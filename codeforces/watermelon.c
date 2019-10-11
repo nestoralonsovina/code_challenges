b35890a9a7d94e32f74d029962fb834aec76bd14
Néstor Alonso Viña <alonsonestor20@gmail.com>
2019-10-11 12:14:00 +0200

    starting in codeforeces

---
+++ codeforces/watermelon.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int weight;
+
+	scanf("%d", &weight);
+
+	if (weight < 3 || weight % 2 != 0)
+		printf("NO");
+	else
+		printf("YES");
+}
---
+++ codeforces/watermelon.py
@@ -0,0 +1,9 @@
+
+w = int(input())
+
+if w < 3:
+    print("NO")
+elif w % 2 == 0:
+    print("YES")
+else:
+    print("NO")
